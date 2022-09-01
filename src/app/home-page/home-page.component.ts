757f4d224b67df1ea7b6b08e4d7e26a21a25fec7
Juers <959155@cognizant.com>
2022-09-01 16:39:10 -0500

    added leaderboards

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AwsGatewayService } from '../aws-gateway.service';
 
 @Component({
   selector: 'app-home-page',
@@ -8,9 +9,23 @@ import { Router } from '@angular/router';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  topLevelClears: any;
+  topPuzzlesSolved: any;
+
+  constructor(private router: Router, private awsGatewayService: AwsGatewayService) { }
 
   ngOnInit(): void {
+    this.awsGatewayService.getLeaderboard("levels").subscribe(
+      (data:any) => {
+        this.topLevelClears = data;
+        console.log(data);
+      }
+    );
+    this.awsGatewayService.getLeaderboard("score").subscribe(
+      (data:any) => {
+        this.topPuzzlesSolved = data;
+      }
+    )
   }
 
 }
